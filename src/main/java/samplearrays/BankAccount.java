1e6e3e3e2a2108b0b900de56fc82312f941a6a62
IBHI Ahmed Yassine <ibhiyassine@gmail.com>
2025-09-29 23:19:51 +0100

    Finished Exercice 3

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -5,25 +5,68 @@ public class BankAccount {
     String name;
     double currentBalance;
     //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    private Double[] transactions;
+    private int transactionIndex;
 
     public BankAccount(String name, int startingBalance){
+        this.name = name;
+        this.currentBalance = startingBalance;
 
+        transactions = new Double[1000];
+        transactionIndex = 0;
     }
 
     public void deposit(double amount){
+        if(amount < 0) {
+            System.out.println("The deposit was unsuccesful due to invalid amount");
+        }
 
+        System.out.println("Mr/Mrs. " + name + " has deposited an amount of " + amount);
+        System.out.println("Old balance is: " + currentBalance);
+
+        // record the transaction 
+        transactions[transactionIndex] = amount;
+        transactionIndex++;
+        // update balance
+        currentBalance += amount;
+
+        System.out.println("New balance is : " + currentBalance );
+        return;
     }
 
     public void withdraw(double amount){
+        if(amount < 0 ) {
+            System.out.println("The withdraw was unsuccesful because of invalid amount");
+            return;
+        }
+        else if(amount > currentBalance) {
+            System.out.println("The withdraw was unsuccesful because the amount requested exceeds the balance");
+            return;
+        }
 
+        System.out.println("Mr/Mrs. " + name + " has withdrawn an amount of " + amount);
+        System.out.println("Old balance is: " + currentBalance);
+
+        // record the transaction 
+        transactions[transactionIndex] = -amount;
+        transactionIndex++;
+        // update balance
+        currentBalance -= amount;
+
+        System.out.println("New balance is : " + currentBalance );
+        return;
     }
 
     public void displayTransactions(){
-
+        System.out.println("Historic of transactions: ");
+        for(int i = 0; i < transactionIndex; i++) {
+            System.out.println("Transaction " + i + ": " + transactions[i]);
+        }
+        return;
     }
 
     public void displayBalance(){
-
+        System.out.println("CurrentBalance : " + currentBalance);
     }
 
     public static void main(String[] args) {
